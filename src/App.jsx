8fd7184869e5d08f646563fe0de47ce530c7b6de
Brian Haller <brianhaller@Brians-MacBook-Pro.local>
2019-11-25 14:02:14 -0500

    added env folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import React, { Component} from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom"
+
+import Login from "./components/login.jsx"
+import FeedContainer from "./components/feedContainer.jsx"
+
 import "./App.css";
 
 class App extends Component{
@@ -10,7 +15,16 @@ class App extends Component{
 
     render () {
         return (
-            <h1>TESTING</h1>
+            <BrowserRouter>
+            <div>
+                <Link to="/"><button>Login</button></Link>
+                <Link to="/home"><button>Main Page</button></Link>
+            </div>
+                <Switch>
+                    <Route exact path="/" component={Login} />
+                    <Route path="/home" component={FeedContainer} />
+                </Switch>
+            </BrowserRouter>
         )
     }
 }
---
+++ src/components/feedContainer.jsx
@@ -0,0 +1,17 @@
+import React, { Component} from "react";
+
+class FeedContainer extends Component{
+    constructor (props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render () {
+        return (
+            <h1>Feed Container</h1>
+        )
+    }
+}
+
+export default FeedContainer;
