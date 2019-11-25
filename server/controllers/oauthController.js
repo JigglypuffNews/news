b797c94b96b98e6f5c99f8b5aed1151529fff2be
Tom Herrmann <tomherrmanndev@gmail.com>
2019-11-25 14:04:22 -0500

    Create oauth controller Co-authored-by: dianewudw <dianewudw@gmail.com>

---
+++ server/controllers/oauthController.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+  tokenLI(req, res, next) { },
+
+}
--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
+const oauthController = require('./controllers/oauthController')
 
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, '../dist')));
+
 
 
 app.listen(3000, () => { console.log('Listening on port 3000!') })
