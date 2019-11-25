5b0fd05f435d8a2c61eae5efdb9595c8f60db10e
Tom Herrmann <tomherrmanndev@gmail.com>
2019-11-25 12:20:43 -0500

    setup server requirements

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser');
 
+app.use(express.static('/dist'));
+
+app.use(bodyParser.json());
 
 app.listen(3000, () => { console.log('Listening on port 3000!') })
