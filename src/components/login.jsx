1a79cb7038202e6dda037764d56ee1a92561f1cb
Joseph Wolensky <jwastoria86@gmail.com>
2019-11-25 20:11:54 -0500

    CSS stylings to login page/ css div improvements

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,37 +1,37 @@
-import React, { Component} from "react";
-import "./componentStyle.css";
-import Logo from "../photos/loginPhoto.png";
+import React, { Component } from 'react';
+import './componentStyle.css';
+import Logo from '../photos/loginPhoto.png';
 
 // SAMPLE OAUTH FLOW
 // https://www.linkedin.com/oauth/v2/accessToken?grant_type=client_credentials&client_id=93r29maplxr58u&client_secret=rA1z8zBOM3yrX123
 
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-class Login extends Component{
-    constructor (props) {
-        super(props);
-        this.state = {
-            
-        }
-        this.handleClick = this.handleClick.bind(this);
-    }
+  handleClick() {
+    fetch('/login');
+  }
 
-    handleClick() {
-        fetch('/login')
-    }
-
-    render () {
-        return (
-            <div id="loginPage">
-                <div id="loginBox">
-                    <h1>Login</h1>
-                    <button onClick={this.handleClick}>LOG IN TO LINKEDIN</button>
-                </div>
-                <div id="logoBox">
-                    <img src={Logo} id="logoPic" height="30%" width="40%" alt="weblogo" />
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="loginPage">
+        <h1>TechMe</h1>
+        <div id="loginBox">
+          {/* <h1 id="mainLoginText">Login</h1> */}
+          <button id="mainLoginButton" onClick={this.handleClick}>
+            LOG VIA LINKEDIN
+          </button>
+        </div>
+        <div id="logoBox">
+          <img src={Logo} id="logoPic" height="30%" width="40%" alt="weblogo" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,35 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   mode: process.env.NODE_ENV,
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
-    contentBase: path.join(__dirname, "./dist"),
-    port: 8080,
+    contentBase: path.join(__dirname, './dist'),
+    port: 8081,
     hot: true,
-    proxy: [{
+    proxy: [
+      {
         context: ['/'],
-        target: 'http://localhost:3000'
-    }]
-  }, 
+        target: 'http://localhost:3000',
+      },
+    ],
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|jpg|gif)$/i,
@@ -40,8 +42,8 @@ module.exports = {
           },
         ],
       },
-    ]
+    ],
   },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
 };
