2a5b27acc8b4599b72ba955bcb46aa4efd069528
Joseph Wolensky <jwastoria86@gmail.com>
2019-11-26 14:46:29 -0500

    merg it

--- src/functions/searchArticles.js
+++ src/functions/searchArticles.js
@@ -1,14 +1,16 @@
-function SearchArticles (articlesArr) {
-    let titles = []
-    for (let i = 0; i < 20; i += 1) {
-        let articleID = articlesArr[i]
-        fetch(`https://hacker-news.firebaseio.com/v0/item/${articleID}.json?print=pretty`)
-        .then(res => res.json())
-        .then(res => titles.push(res))
-    }
-    console.log(titles)
+function SearchArticles(articlesArr) {
+  const titles = [];
+  for (let i = 0; i < 20; i += 1) {
+    const articleID = articlesArr[i];
+    fetch(
+      `https://hacker-news.firebaseio.com/v0/item/${articleID}.json?print=pretty`
+    )
+      .then(res => res.json())
+      .then(res => titles.push(res));
+  }
+  console.log(titles);
 
-    return titles
+  return titles;
 }
 
 export default SearchArticles;
