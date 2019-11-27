78d7b6cab8832a26bbd4976c5142e78393c0c612
Joseph Wolensky <jwastoria86@gmail.com>
2019-11-26 20:08:56 -0500

    updated CSS stylings, dynamic rendering of API info to DOM, Began work on using react hook in feedBox to turn feedbox display on and off via style attr modification

--- src/components/feedBox.jsx
+++ src/components/feedBox.jsx
@@ -1,5 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import genericImage from '../photos/feedImages/motherBoard.jpg';
 
-const FeedBox = () => <h1>This is the feed box</h1>;
+function FeedBox(props) {
+  const [isDivVisible, changeView] = useState('block');
+  return (
+    <>
+      <div
+        className="feedBoxShade"
+        style={{ display: isDivVisible }}
+        onMouseOut={() => changeView((isDivVisible = 'block'))}
+        onMouseOver={() => changeView((isDivVisible = 'none'))}
+      >
+        <div id="feedBoxOuter">
+          <div onClick={props.leftArrowClick} className="arrow-left"></div>
+          <div id="feedBoxInner">
+            {/* <div className="imageContainer"></div> */}
+
+            <img
+              className="imageContainer"
+              src={props.imageURL}
+              alt="circuit board"
+            />
+            <div className="textFeedBox">
+              <div className="mainTitleFeed">
+                <h2>{props.title}</h2>
+              </div>
+              <div className="feedBoxSummary">
+                <div className="feedBoxTextContainer">
+                  <h6>{props.link}</h6>
+                  <h5>{props.author}</h5>
+                  <p>{props.summary}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div onClick={props.rightArrowClick} className="arrow-right"></div>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default FeedBox;
--- src/components/feedContainer.jsx
+++ src/components/feedContainer.jsx
@@ -4,34 +4,63 @@ import FeedControl from './feedControl';
 import ConnectHackerNewsAPI from '../functions/connectHackerNewsAPI';
 import SearchArticles from '../functions/searchArticles';
 
-
 class FeedContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      topics: ["graphql", "GitHub", "API", "Javascript", "HTML", "World", "open-source"]
+      topics: [
+        'graphql',
+        'GitHub',
+        'API',
+        'Javascript',
+        'HTML',
+        'World',
+        'open-source',
+      ],
+      articles: [],
     };
-    this.handleClick = this.handleClick.bind(this)
+    this.leftArrowClick = this.leftArrowClick.bind(this);
+    this.rightArrowClick = this.rightArrowClick.bind(this);
   }
 
   componentDidMount() {
-    ConnectHackerNewsAPI()
-    .then(res => SearchArticles(res, this.state.topics));
+    fetch(
+      `https://newsapi.org/v2/everything?q=graphql&language=en&pagesize=100&from=2019-11-26&sortBy=popularity&apiKey=4ffc6971bc1e48fcbb98a57331bbebd4`
+    )
+      .then(res => res.json())
+      .then(res => this.setState({ articles: res.articles }))
+      .then(() => console.log('lol', this.state.articles));
   }
 
-  handleClick () {
-    fetch(`https://newsapi.org/v2/everything?q=graphql&language=en&pagesize=100&from=2019-11-26&sortBy=popularity&apiKey=4ffc6971bc1e48fcbb98a57331bbebd4`)
-    .then(res => res.json())
-    .then(res => console.log(res))
+  leftArrowClick() {
+    console.log('LEFT ARROW CLICKED');
+  }
+
+  rightArrowClick() {
+    console.log('RIGHT ARROW CLICKED');
   }
 
   render() {
+    const newsFeedToRender = [];
+    for (let i = 0; i < this.state.articles.length; i += 1) {
+      newsFeedToRender.push(
+        <FeedBox
+          author={this.state.articles[i].author}
+          title={this.state.articles[i].title}
+          imageURL={this.state.articles[i].urlToImage}
+          link={this.state.articles[i].url}
+          summary={this.state.articles[i].description}
+          leftArrowClick={this.leftArrowClick}
+          rightArrowClick={this.rightArrowClick}
+        />
+      );
+    }
+
     return (
       <>
         <h1>Feed Container</h1>
-        <button onClick={this.handleClick}>News API</button>
-        <FeedBox />
         <FeedControl />
+        {newsFeedToRender}
       </>
     );
   }
